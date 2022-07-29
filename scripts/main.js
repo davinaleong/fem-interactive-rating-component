9aecac717f9352a5cec7d77990b5efaee864307d
Davina Leong <davina.leong.sy@gmail.com>
2022-07-29 11:18:37 +0800

    Done with rating form

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
-const formSection = document.querySelector(".form-section");
-const thankYouSection = document.querySelector(".thank-you-section");
+const formSection = document.querySelector("#formSection");
+const thankYouSection = document.querySelector("#thankYouSection");
+
+const ratingForm = document.querySelector("#ratingForm");
+const formStatus = document.querySelector("#formStatus");
+const userRating = document.querySelector("#userRating");
+
+const errorMessages = {
+  required: "Please submit a rating.",
+};
+
+const clearForm = () => {
+  formStatus.innerHTML = "";
+  userRating.innerHTML = "";
+};
+
+ratingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  clearForm();
+
+  const rating = ratingForm.elements["rating"];
+  const value = rating.value;
+
+  if (value) {
+    userRating.innerHTML = value;
+    formSection.setAttribute("data-close", true);
+    thankYouSection.setAttribute("data-open", true);
+  } else {
+    formStatus.innerHTML = errorMessages.required;
+  }
+});
